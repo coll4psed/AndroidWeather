de0bb8815bfb3ad2d58b98b3c567dc82937c919e
Дмитрий Орлов <dmitry.orlov00@yandex.ru>
2022-10-08 11:14:34 +0300

    Добавьте файлы проекта.

---
+++ AndroidTest/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace AndroidTest;
+
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public partial class MainPage : ContentPage
+{
+	string city = "Saint Petersburg";
+	readonly string key = "8c765466d34a1448fa1e7bac879dbd48";
+	string url;
+	HttpClient httpClient;
+	public MainPage()
+	{
+		url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=metric&lang=ru";
+		httpClient = new HttpClient();
+        InitializeComponent();
+	}
+
+	private async void BtnClickCity(object sender, EventArgs e)
+	{
+		HttpResponseMessage response = await httpClient.GetAsync(url);
+		var json = JObject.Parse(await response.Content.ReadAsStringAsync());
+		var jsonWeather = JObject.Parse(json.GetValue("main").ToString());
+		LabelForWeather.Text = $"Температура: {jsonWeather.GetValue("temp")}\n" +
+			$"Ощущается как: {jsonWeather.GetValue("feels_like")}\n" +
+			$"Давление: {jsonWeather.GetValue("pressure")}";
+	}
+
+	private void BtnClickSpb(object sender, EventArgs e)
+	{
+
+	}
+}
+
---
+++ AndroidTest/Weather.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidTest
+{
+    public class Weather
+    {
+        public double temp { get; set; }
+        public double pressure { get; set; }
+        public int humidity { get; set; }
+        public int temp_min { get; set; }
+        public int temp_max { get; set; }
+    }
+}
